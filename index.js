caef439f17e2ed3fb0ec5d2f3ad2abffd553b2e8
Tom Burnell <tombburnell@gmail.com>
2017-11-17 10:24:04 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+    env2obj: env2obj
+};
+
+function env2obj(object, opts) {
+    var PREFIX = opts.prefix || "CONF_";
+    var delim = opts.delim || "__";
+    Object.keys(process.env).forEach(key => {
+        if (key.indexOf(PREFIX) === 0) {
+            var tmp = config;
+            key.slice(PREFIX.length).split(delim).forEach((prop, i, all) => {
+                if ((i + 1) < all.length) {
+                    tmp = tmp[prop] || (tmp[prop] = {});
+                } else {
+                    var value = process.env[key];
+                    // either the initial config is an Array
+                    // or the string contains a,b,b but not a\,b\,c *
+                    // * to allow use of comma in single value
+                    if (tmp[prop] instanceof Array || /[^\\],/.test(value)) {
+                        tmp[prop] = value.split(/\s*,\s*/);
+                    } else {
+                        // in other cases drop the possible \ before ,
+                        tmp[prop] = value.replace(/[^\\],/g, ',');
+                    }
+                }
+            });
+        }
+    });
+}
